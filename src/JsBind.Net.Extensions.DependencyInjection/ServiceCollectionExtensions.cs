688821c2566a3653d647d332212958b9e9868823
Ming Yau Lee <lee_mingyau@hotmail.com>
2021-08-25 10:02:07 +0800

    Generate documentation for dependency injection extension package

--- src/JsBind.Net.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/JsBind.Net.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -5,11 +5,25 @@ using JsBind.Net.Configurations;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
+    /// <summary>
+    /// Extension method for service registration.
+    /// </summary>
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds the required services for using JsBind.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add the services to.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
         public static IServiceCollection AddJsBind(this IServiceCollection services)
             => services.AddJsBind(null);
 
+        /// <summary>
+        /// Adds the required services for using JsBind.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add the services to.</param>
+        /// <param name="configAction">The action to configure JsBind.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
         public static IServiceCollection AddJsBind(this IServiceCollection services, Action<IJsBindOptionsConfigurator>? configAction)
         {
             var configurator = GetRegisteredConfigurator(services);
