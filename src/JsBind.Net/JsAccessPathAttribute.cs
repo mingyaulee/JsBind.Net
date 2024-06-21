b160b5b98bb323f1c3546af4bb50f84ad36db49d
mingyaulee <lee_mingyau@hotmail.com>
2024-06-21 23:34:15 +0100

    Add JsAccessPath attribute

---
+++ src/JsBind.Net/JsAccessPathAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JsBind.Net
+{
+    /// <summary>
+    /// Represents the JS access path for the object, intended to be used by reflection.
+    /// </summary>
+    /// <param name="accessPath">The JS access path.</param>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Struct | AttributeTargets.Method | AttributeTargets.Property)]
+    public class JsAccessPathAttribute(string accessPath) : Attribute
+    {
+        /// <summary>
+        /// The JS access path.
+        /// </summary>
+        public string AccessPath => accessPath;
+    }
+}
